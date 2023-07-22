71d6f64937a80d1745dc67843a3dbc98a361a7d7
Oleh Buhaichuk <oleh.buhaichuk@gmail.com>
2023-07-22 15:18:22 +0300

    start learning next.js

---
+++ app/page.js
@@ -0,0 +1,19 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className='flex  flex-col items-center justify-between p-24'>
+      <ul className='flex flex-col min-h-screen mb-50'>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+        <li>6</li>
+        <li>7</li>
+        <li>8</li>
+        <li>9</li>
+      </ul>
+    </main>
+  );
+}
---
+++ app/posts/new/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>NewPosts</div>;
+};
+
+export default page;
