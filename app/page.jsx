3c7ff7e86996674bc332f3b3b61db43b0ff9406c
Oleh Buhaichuk <oleh.buhaichuk@gmail.com>
2023-07-25 23:47:51 +0300

    add scc and some configs

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <section className='w-full flex-center flex-col'>
+      <h1 className='head_text text-center'>Discover & Share</h1>
+      <br className='max-md:hidden' />
+    </section>
+  );
+};
+
+export default Home;
