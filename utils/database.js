cc47b900d8fa9b465b7a530954f2a6f93d05437a
Oleh Buhaichuk <oleh.buhaichuk@gmail.com>
2023-07-27 00:39:52 +0300

    fix

--- utils/database.js
+++ utils/database.js
@@ -20,6 +20,6 @@ export const connectToDB = async () => {
     isConnected = true;
     console.log("MongoDB connected");
   } catch (error) {
-    console.log(error);
+    console.log("error" + error);
   }
 };
